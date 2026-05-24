b83f2cb2a3bb2beee6e2e579e39344874e27d26d
valeor0224 <00206022@uca.edu.sv>
2026-05-24 15:49:27 -0600

    feat: add data to json response

--- app/parser/models.py
+++ app/parser/models.py
@@ -3,6 +3,23 @@ from dataclasses import dataclass
 
 @dataclass
 class Vulnerability:
+
     severity: str
+
+    cvss_score: float
+
     cve: str
+
     host: str
+
+    port: str
+
+    name: str
+
+    family: str
+
+    description: str
+
+    impact: str
+
+    solution: str
